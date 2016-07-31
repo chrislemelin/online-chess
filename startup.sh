ef203b5954cc8c7086607aeffbe10e95cecbadf6
Chris Lemelin <clemelin2@gmail.com>
2016-07-31 16:53:00 -0400

    made the startup.sh better

--- startup.sh
+++ startup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 
+tmux kill-session -t my-session
 
 tmux new -d -A -s my-session
-tmux split-window -h 
+tmux split-window -h
 tmux split-window -v -t my-session:0.1
-tmux send -t my-session:0.0 "./server/server 5000" Enter
-tmux send -t my-session:0.1 "./client/client localhost 5000" Enter "chris" Enter
-tmux send -t my-session:0.2 "./client/client localhost 5000" Enter "bob" Enter
+tmux send -t my-session:0.0 "./server/server 5001" Enter
+sleep 1
+tmux send -t my-session:0.1 "./client/client localhost 5001" Enter
+tmux send -t my-session:0.1 "chris" Enter
+tmux send -t my-session:0.2 "./client/client localhost 5001" Enter 
+tmux send -t my-session:0.2 "bob" Enter
 tmux attach -t my-session
-
